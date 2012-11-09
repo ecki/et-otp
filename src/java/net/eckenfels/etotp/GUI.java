e9949d549667705a4f3a7a5bf8a072f8c7ee56df
Bernd Eckenfels <bernd@eckenfels.net>
2012-11-09 02:30:51 +0100

    Removed compiler warning.

--- src/java/net/eckenfels/etotp/GUI.java
+++ src/java/net/eckenfels/etotp/GUI.java
@@ -13,7 +13,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileInputStream;
